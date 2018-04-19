3d43460d44554521670d661a87f901baacfd4761
lbenychou <lbenychou@egencia.com>
2018-04-19 11:27:40 +0200

    added a helper

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ const mapping = ['', 'ABC', 'DEF', 'GHI', 'JKL', 'PQRS', 'TUV', 'WXYZ', '', '']
 Array.prototype.flatMap = function(){ return this.reduce((acc, value) => acc.concat(value), [])}
 Array.prototype.without = function(value){ return [...this.slice(0, this.indexOf(value)), ...this.slice(this.indexOf(value) + 1)]}
 
+const numberOfDigitsIn = number => Math.max(0, Math.floor(Math.log10(number)) + 1)
+
 export default number => {throw new Error('To be implemented')}
