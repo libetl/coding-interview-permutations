9fd2f36aad2195b863c05810595573959037323c
lbenychou <lbenychou@egencia.com>
2018-04-19 11:19:00 +0200

    initial version

--- __tests__/permutations-test.js
+++ __tests__/permutations-test.js
@@ -9,35 +9,32 @@ describe('permutation module', function() {
         //Given
         const forATooLowNumber = 0
         //when
-        const theResult = permutationFunction(forATooLowNumber)
         //then
-        theResult.should.have.lengthOf(0)
+        //theResult.should.have.lengthOf(?)
+        fail()
     })
 
     it('should render no permutation for 9', function () {
         //Given
         const forA0MappingNumber = 9
         //when
-        const theResult = permutationFunction(forA0MappingNumber)
         //then
-        theResult.should.have.lengthOf(0)
+        fail()
     })
 
     it('should not understand a number with unauthorized digits as in 912', function () {
         //Given
         const forANumberContaining1DigitWithoutPermutation = 912
         //when
-        const theResult = permutationFunction(forANumberContaining1DigitWithoutPermutation)
         //then
-        theResult.should.have.lengthOf(0)
+        fail()
     })
 
     it('should render 162 permutations for 432', function () {
         //Given
         const forAReasonableNumber = 432
         //when
-        const theResult = permutationFunction(forAReasonableNumber)
         //then
-        theResult.should.have.lengthOf(162)
+        fail()
     })
 })
