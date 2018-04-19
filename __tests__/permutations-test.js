521e1da0a20ad949599bff031f5ab794f84882c0
lbenychou <lbenychou@egencia.com>
2018-04-19 13:41:03 +0200

    applicant-1

--- __tests__/permutations-test.js
+++ __tests__/permutations-test.js
@@ -1,7 +1,7 @@
 import permutationFunction from '../src/index'
 import chai from 'chai'
 
-beforeEach(() => chai.should())
+beforeEach(() => chai.expect)
 
 describe('permutation module', function() {
 
@@ -9,32 +9,53 @@ describe('permutation module', function() {
         //Given
         const forATooLowNumber = 0
         //when
+        const theResult = permutationFunction(forATooLowNumber);
         //then
-        //theResult.should.have.lengthOf(?)
-        fail()
+        expect(theResult).toEqual([]);
+        //fail()
     })
 
     it('should render no permutation for 9', function () {
         //Given
         const forA0MappingNumber = 9
         //when
+        const theResult = permutationFunction(forA0MappingNumber);
         //then
-        fail()
+        expect(theResult).toEqual([]);
     })
 
-    it('should not understand a number with unauthorized digits as in 912', function () {
+    it('should render no permutation for 5', function () {
         //Given
-        const forANumberContaining1DigitWithoutPermutation = 912
+        const forA0MappingNumber = 5
         //when
+        const theResult = permutationFunction(forA0MappingNumber);
         //then
-        fail()
+        expect(theResult).toEqual(['P', 'Q', 'R', 'S']);
     })
 
-    it('should render 162 permutations for 432', function () {
-        //Given
-        const forAReasonableNumber = 432
-        //when
-        //then
-        fail()
-    })
+        it('should render no permutation for 35', function () {
+            //Given
+            const forA0MappingNumber = 35
+            //when
+            const theResult = permutationFunction(forA0MappingNumber);
+            //then
+            expect(theResult).toEqual(["PGHI", "QGHI", "RGHI", "SGHI"]);
+        })
+    /*
+            it('should not understand a number with unauthorized digits as in 912', function () {
+                //Given
+                const forANumberContaining1DigitWithoutPermutation = 912
+                //when
+                //then
+                fail()
+            })
+
+            it('should render 162 permutations for 432', function () {
+                //Given
+                const forAReasonableNumber = 432
+                //when
+                //then
+                fail()
+            })
+            */
 })
--- src/index.js
+++ src/index.js
@@ -8,5 +8,30 @@ Array.prototype.without = function(value){ return [...this.slice(0, this.indexOf
 const numberOfDigitsIn = number => Math.max(0, Math.floor(Math.log10(number)) + 1)
 
 
+               const numberRec = (digits, n) => {
+                   if (n === 0) {
+                       return [];
+                   }
+                   if (n === 1) {
+                       return mapping[digits[0]].split('');
+                   }
+                   const array = mapping[digits[n-1]].split('');
+                   const result = [];
+                   for (const e of array) {
+                       result.push([e].concat(numberRec(digits, n-1)).join(''));
+                   }
+                   return result;
+               }
+
 //This module should compute all the possible permutations for a number, which is translated into an array of digits
-export default number => {throw new Error('To be implemented')}
+export default number => {
+    const digits = [];
+    const str = `${number}`;
+    for (let i = 0; i < str.length; i++) {
+        const digit = Number.parseInt(str[i]);
+        if (mapping[digit] !== '')
+            digits.push(str[i]);
+    }
+    return numberRec(digits, digits.length);
+
+}
